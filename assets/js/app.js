2ed00e53bd66aa0fd21d2593b8246334b4c7ebc7
Ariana Cabana <acabana.0904@gmail.com>
2017-06-18 16:42:04 -0500

    index

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,59 +4,30 @@
 //   alert("holaaaa");
 // });
 
-
 const render = (root) => {
   root.empty();
 
   const wrapper = $('<div class="wrapper"></div>');
   wrapper.append(Header());
-  if (state;selectedMovie == null){
-  wrapper.append(MovieGrid( _ =>{
-    render(root);
-  });
-} else {
-
-}
-)
-
+  if (state.selectedMovie == null) {
+    wrapper.append(MovieGrid( _ => {
+      render(root);
+    }));
+  } else{
+    wrapper.append(MovieDetails( _ => {
+      render(root);
+    }));
+  }
   root.append(wrapper);
 };
 
 const state = {
-  movies: [],
+  movies: movies,
   selectedMovie: null;
 }
 
+$( _ => {
 
-// // otro js
-// use strict
-//
-// const movieItem = (movie) => {
-//   const item = $('<div class="movie"></div>');
-//   const image = $(<img src="${movie.thumb}" alt="$(movie.title)">);
-//
-//   const p = $('<p>' + movie.title + </p>);
-//
-//   item.append(image);
-//   item.append(p);
-//
-//   return item
-// }
-//
-//
-// const MovieGrid = () => {
-//   const movieContainer = $('<div class="movieContainer"></div>');
-//   state.movies.forEach((movie) => {
-//     movieContainer.append(mpvieItem(movie));
-//   });
-// };
-
-// // moovie.js
-// [{
-//   "title":"Wonder Woman",
-//   "thumb": "url.jpg"
-// },
-// {
-//   "title":"Piratas del Caribe",
-//   "thumb": "url.jpg"
-// }];
+  const root = $('.root');
+  render(root);
+});
